7da2ac030c88afd1d5c431179ae2e9a62d822004
Justin <justinbuhl93@gmail.com>
2021-07-12 21:51:09 -0500

    Cache last valid payment in clipboard

--- app/utils/clipboard.ts
+++ app/utils/clipboard.ts
@@ -1,15 +1,21 @@
 import Clipboard from "@react-native-community/clipboard"
+import { ApolloClient } from "@apollo/client"
 
 import { validPayment } from "./parsing"
 import { modalClipboardVisibleVar, walletIsActive } from "../graphql/query"
 import { Token } from "./token"
-import { ApolloClient } from "@apollo/client"
+import { loadString, saveString } from "./storage"
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const checkClipboard = async (client: ApolloClient<object>): Promise<void> => {
+  const LAST_CLIPBOARD_PAYMENT = "lastClipboardPayment"
   const clipboard = await Clipboard.getString()
 
   if (!walletIsActive(client)) {
+    return
+  }
+
+  if (clipboard === (await loadString(LAST_CLIPBOARD_PAYMENT))) {
     return
   }
 
@@ -19,4 +25,5 @@ export const checkClipboard = async (client: ApolloClient<object>): Promise<void
   }
 
   modalClipboardVisibleVar(true)
+  saveString(LAST_CLIPBOARD_PAYMENT, clipboard)
 }
